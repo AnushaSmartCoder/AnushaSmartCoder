d11a916fe01c3aa69701e997fc3fe8efea3bd9f1
AnushaSmartCoder <99254022+AnushaSmartCoder@users.noreply.github.com>
2022-02-11 16:17:41 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,5 @@
+def f1():                                #write program to add an element to the list by using  append functions 
+    x=["apple","banana","cat",2,9]
+    x.append("z")
+    print(x)
+f1()
