269190d42299bf8ee5727e7e166234728ae3a05c
AnushaSmartCoder <99254022+AnushaSmartCoder@users.noreply.github.com>
2022-02-11 16:16:51 +0530

    Add files via upload

---
+++ space latters.py
@@ -0,0 +1,9 @@
+def splitting(string):
+    for each in string:
+        if(each ==","):
+            pass
+        else:
+            print(each,end=" ")
+s="python,interpreted,language"
+splitting(s)
+    
