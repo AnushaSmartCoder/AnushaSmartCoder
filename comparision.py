463ab805b0893257c9521bed84059ae4f1cb20e9
AnushaSmartCoder <99254022+AnushaSmartCoder@users.noreply.github.com>
2022-02-15 16:21:55 +0530

    Add files via upload

---
+++ comparision.py
@@ -0,0 +1,10 @@
+def f():
+	x=10
+	y=9
+	print(x==y)
+	print(x!=y)
+	print(x>y)
+	print(x<y)
+	print(x<=y)
+	print(x>=y)
+f()
---
+++ continue.py
@@ -0,0 +1,9 @@
+def f():
+	i=3
+	while i>0:
+		print("easy language")
+		if i==3:
+			print("hello python")
+			break
+		i=i+1
+f()
